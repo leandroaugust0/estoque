a4cc2bbe188c08c03ae065ce37d7cd52b1b12df4
leandroaugust0 <64994115+leandroaugust0@users.noreply.github.com>
2022-01-25 21:15:55 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (request, response) => {
+  return response.json({
+    data: "Testando"
+  });
+});
+
+export { router };
